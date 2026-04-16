5982ea5d69af6c14667e7e274d85683121ec5faf
Denys <nesfow@gmail.com>
2026-04-16 22:31:24 +1000

    refactor: changing Location object to GetLocationDto in EventDetailsDto

--- src/Application/Features/Event/GetEventDetails/EventDetailsDto.cs
+++ src/Application/Features/Event/GetEventDetails/EventDetailsDto.cs
@@ -1,3 +1,4 @@
+using BookingSystemApi.Application.Features.Location.GetLocation;
 using BookingSystemApi.Domain.Entities;
 using BookingSystemApi.Domain.Enums;
 
@@ -11,5 +12,5 @@ public class EventDetailsDto
     public DateTime Date { get; set; }
     public Dictionary<SeatType, int> AllSeats { get; set; } = [];
     public Dictionary<SeatType, int> AvailableSeats { get; set; } = [];
-    public Location Location { get; set; } = null!;
+    public GetLocationDto Location { get; set; } = null!;
 }
