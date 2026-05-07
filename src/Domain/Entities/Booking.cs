9ffbda4b120de854f1dd0f3716930c37756435e3
Denys <nesfow@gmail.com>
2026-05-07 22:02:05 +1000

    refactor: adding default value for BookingDate - UTC now

--- src/Domain/Entities/Booking.cs
+++ src/Domain/Entities/Booking.cs
@@ -7,7 +7,7 @@ public class Booking
     public int Id { get; set; }
     public int UserId { get; set; }
     public int EventId { get; set; }
-    public DateTimeOffset BookingDate { get; set; }
+    public DateTimeOffset BookingDate { get; set; } = DateTimeOffset.UtcNow;
     public decimal Price { get; set; }
     public BookingStatus BookingStatus { get; set; } = BookingStatus.Pending;
 
